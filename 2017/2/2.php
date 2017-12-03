a3b5f14a6f3364f61bb01479f105cbbeabd5307b
Brad Mallett <bradmallett@gmail.com>
2017-12-03 11:23:14 -0500

    day 2 part 2

--- 2017/2/2.php
+++ 2017/2/2.php
@@ -17,7 +17,35 @@ function partTwo($input)
 {
 	$sum = 0;
 	
+	$raw	= getInputFile($input);
+	$data	= parseTabbedDelimintedInput($raw);
+	
+	foreach ($data as $row)
+	{
+		for ($i = 0; $i < count($row); $i++)
+		{
+			for ($j = 0; $j < count($row); $j++)
+			{
+				if ($i == $j) continue;
+				
+				$val = $row[$i] / $row[$j];
+				
+				if (isWholeNumber($val))
+				{
+					$sum += $val;
+					echo $row[$i] . " / " . $row[$j] . " = " . $row[$i] / $row[$j] . "<br />";
+				}
+			}
+		}
+	}
+	
 	return $sum;
+}
+
+// holy cow, I hate both PHP and myself right now.
+function isWholeNumber($num)
+{
+	return fmod($num, 1) == 0;
 }
 
 function partOne($input)
