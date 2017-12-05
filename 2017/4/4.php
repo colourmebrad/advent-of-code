bf542d14b1261156ba0b75dfa38ed2a4e568de72
Brad Mallett <bradmallett@gmail.com>
2017-12-04 22:31:05 -0500

    day 4, part two (answer is too low)

--- 2017/4/4.php
+++ 2017/4/4.php
@@ -15,7 +15,49 @@ else if ($_GET["part"] == 2)
 
 function partTwo($input)
 {
-	echo "0";
+	$raw		= getInputFile($input);
+	$data		= parseSpaceDelimintedInput($raw);
+	
+	echo "<pre>";
+	//print_r($data);
+	echo "</pre>";
+	
+	$numValid	= 0;
+	foreach ($data as $pass)
+	{
+		if (partTwoValidPassphrase($pass))
+		{
+			$numValid++;
+		}
+	}
+	
+	echo $numValid;
+}
+
+function partTwoValidPassphrase($pass)
+{
+	if (count(array_unique($pass)) != count($pass))
+	{
+		return false;
+	}
+	
+	for ($i = 0; $i < count($pass)-1; $i++)
+	{
+		for ($j = $i+1; $j < count($pass); $j++)
+		{
+			if (isAnagram($pass[$i], $pass[$j]))
+			{
+				return true;
+			}
+		}
+	}
+	
+	return false;
+}
+
+function isAnagram($a, $b)
+{
+    return count_chars($a, 1) == count_chars($b, 1);
 }
 
 function partOne($input)
