1b6380923503328f666aa587fc1f553cbcaead10
Brad Mallett <bradmallett@gmail.com>
2017-12-22 17:23:50 -0500

    day 6, part 2

--- 2017/6/6.php
+++ 2017/6/6.php
@@ -16,9 +16,65 @@ else if ($_GET["part"] == 2)
 function partTwo($input)
 {
 	$raw	= getInputFile($input);
+	
 	$blocks	= parseFile($raw);
 	
-	return 0;
+	$blockCount	= count($blocks);
+	
+	$configs	= array($raw);	// this first config is the one given in the file
+	
+	while (true)
+	{
+		$largest = -1;
+		$largestIndex = -1;
+		for ($i = 0; $i < $blockCount; $i++)
+		{
+			if ($blocks[$i] > $largest)
+			{
+				$largest = $blocks[$i];
+				$largestIndex = $i;
+			}
+		}
+		
+		$redCount = $largest;
+		
+		$blocks[$largestIndex] = 0;
+		
+		$redIndex = $largestIndex;
+		
+		while ($redCount > 0)
+		{
+			$redIndex = $redIndex == $blockCount - 1 ? 0 : $redIndex + 1;
+
+			$blocks[$redIndex]++;
+			
+			$redCount--;
+		}
+		
+		$thisConfig = implode("	", $blocks);
+		
+		$foundIndex = array_search($thisConfig, $configs);
+		
+		if ($thisConfig == "2	4	1	2")
+		{
+			echo "foundIndex is $foundIndex<br />";
+		}
+		
+		$configs[] = $thisConfig;
+
+		if ($foundIndex !== false)
+		{
+			echo "loopCycles = " . count($configs) . " - $foundIndex<br />";
+			$loopCycles = count($configs) - 1 - $foundIndex;
+			break;
+		}
+	}
+	
+	echo "<pre>";
+	print_r($configs);
+	echo "</pre>";
+	
+	return $loopCycles;
 }
 
 function partOne($input)
